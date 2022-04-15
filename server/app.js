39edec025f0adbc1539006d24db6c6884813199a
erwinschrodinger1 <sunnynirjal@gmail.com>
2022-04-15 20:40:42 +0545

    with build

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const path = require("path");
 const app = express();
 require("dotenv").config({ path: "/config.env" });
 URL =
@@ -15,6 +16,11 @@ app.use(
 );
 app.use(cookieParser());
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "../server/client/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../server/client/build"));
+});
 
 app.use("/", require("./routes/user"));
 app.use("/", require("./routes/oauth"));
